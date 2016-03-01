b185512c8e8364f9929e267a96b680baa550c12a
Vadim Eisenberg <vadime@il.ibm.com>
2016-03-01 07:54:52 +0200

    IBM-Swift/Kitura#84 added initial implementation for MustacheTemplateEngine

---
+++ Sources/KituraMustacheTemplateEngine.swift
@@ -0,0 +1,30 @@
+/**
+ * Copyright IBM Corporation 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import KituraTemplateEngine
+import Mustache
+
+public class KituraMustacheTemplateEngine: TemplateEngine {
+    public var fileExtension: String? { return "mustache" }
+    public init() {}
+
+    public func render(filePath: String, context: [String: Any]) throws -> String {
+        let template = try Template(path: filePath)
+        // currently use type conversion to [String: AnyObject], works only in OS X
+        let contextBox = Box(context as! [String: AnyObject])
+        return try template.render(contextBox)
+    }
+}
