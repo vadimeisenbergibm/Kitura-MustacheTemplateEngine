9e4642b7aea1f406c18d3ab0a3dcfff42c6e68a7
Shmuel Kallner <kallner@il.ibm.com>
2016-09-22 16:09:50 +0300

    updated dependency versions in Package.swift
    set version of https://github.com/IBM-Swift/Kitura-TemplateEngine.git to 1.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 
 let package = Package(
     name: "KituraMustache",
-    dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 32)])
+    dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 0)])
 
 #if os(OSX)
     package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/GRMustache.swift.git",
