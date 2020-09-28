9e504b3caa9d80689b2950dcf93ca595f0d7c4bb
Fouad Hatem <fouadhatem@icloud.com>
2020-09-29 02:45:47 +0300

    Update package references

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
- * Copyright IBM Corporation 2016, 2018
+ * Copyright IBM Corporation and the Kitura project authors 2016-2020
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,8 +29,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/IBM-Swift/GRMustache.swift.git", from: "1.7.4")
+        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.0"),
+        .package(url: "https://github.com/Kitura/GRMustache.swift.git", from: "1.7.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
